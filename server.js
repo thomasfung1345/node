45b58e6943d7d8532ba5854a77bb39288c731fe5
Dennis Wong <Dennis Wong>
2016-11-29 02:13:05 +0800

    changed by dennis

--- server.js
+++ server.js
@@ -10,15 +10,20 @@ var bodyParser = require('body-parser');
 
 app.use(session({
 	name: 'session',
-	keys: ['name_id']
+	keys: ['key1','key2']
 }));
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
+app.get("/login", function(req,res){
+	res.render('index');
+});
 
-app.post("/read", function(req,res) {
+app.post("/login", function(req,res) {
 	/*MongoClient.connect(mongourl, function(err, db) {
 		assert.equal(err,null);
 		console.log('Connected to MongoDB\n');
@@ -26,19 +31,108 @@ app.post("/read", function(req,res) {
 		console.log('read page required');
 		res.render('read', { title: 'Hey', message: 'Hello there!' });
 	})*/
-	console.log("login with " + req.body.id);
-	//db.collection('')
-	res.render('read', { title: 'Hey', message: 'Hello there!' });
+	console.log('logging in......');
+	MongoClient.connect(mongourl, function(err, db) {
+		acc = req.body.id;
+		pwd = req.body.pw;
+		db.collection('account').findOne({acc:acc, pwd:pwd}, function(err, user) {
+			if(err)
+				throw err;
+			
+			if(!user){
+				res.redirect('/login');
+				console.log("a login attempt failed");
+			}
+			else{
+				id = user._id;
+				req.session.uid = id;
+				res.redirect('/read');
+			}
+			
+			
+		});
+		db.close();
+	});
+});
+
+app.get("/read", function(req,res) {
+	if(!req.session.uid)
+		res.redirect('/');
+	else{
+		MongoClient.connect(mongourl, function(err, db) {
+			db.collection('restaurant').find({},{name:1,_id:1}).toArray(function(err,items){
+				res.render('read',{list: items});
+			});
+		});
+	}
 });
 
 app.get("/", function(req,res) {
-	MongoClient.connect(mongourl, function(err, db) {
-		assert.equal(err,null);
-		console.log('Connected to MongoDB\n');
-		db.close();
-		console.log('login page required');
-		res.render('index', { title: 'Hey', message: 'Hello there!' });
-	})
+	if(!req.session.uid)
+		res.render('index');
+	else{
+		res.redirect('read');
+	}
 });
+
+
+app.get("/new", function(req,res) {
+	res.render('new');
+});
+
+app.get("/rate", function(req,res) {
+	rid = req.query._id;
+	res.render('rate', {"rid":rid});
+});
+
+app.post("/rateProcess", function(req,res) {
+	uid = req.session.uid;
+	score = req.body.rate;
+	rid = req.query._id;
+	console.log(rid);
+	MongoClient.connect(mongourl, function(err, db) {
+		
+		db.collection('restaurant').findOne({"_id":ObjectId(rid) , "rate":{$elemMatch:{"user":uid}}} , function(err,match){
+			if(!match){
+				db.collection('restaurant').update({"_id":ObjectId(rid)},{$push:{"rate":{"user":uid,"score":score}}},function(err,object){
+					if(err){
+						console.error(err.message);
+					}
+					else{
+						db.close();
+						res.redirect('/display?_id='+rid);
+					}
+					
+				});
+			}else{
+				db.close();
+				res.redirect('/display?_id='+rid);
+			}
+		});
+	});
+	//res.render('rate');
+});
+
+app.get("/display", function(req,res) {
+	rid = req.query._id;
+	if(!req.session.uid)
+		res.redirect('/');
+	else{
+		MongoClient.connect(mongourl, function(err, db) {
+			db.collection('restaurant').findOne({"_id":ObjectId(rid)},function(err,items){
+				db.collection('account').findOne({"_id":ObjectId(items.uid)},{acc:1},function(err,creatorName){
+					res.render('display',{list: items, creatorName:creatorName.acc});
+				});
+			});
+		});
+		
+	}
+	
+	
+	
+});
+
+
+
 
 app.listen(process.env.PORT || 8099);
